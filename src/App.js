3900fb9b604c4f21da573473c08fa6e6fc902acc
Zied-F <62475880+Zied-F@users.noreply.github.com>
2020-05-15 14:11:20 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import image from './assets/imageInSrc.jpg';
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+    
+           <div style = {{
+           border : "solid 1px black",
+           maxWidth : "100vw"
+           }}>
+                <h1 className ="title">Your name here</h1>
+                <img src={image} alt ="src" />
+                <br></br>
+                <img src = "/imageInPublic.jpg" alt ="public" />
+
+          </div>
+          
+          <video width="320" height="240" controls>
+          <source src="/video.mp4" type="video/mp4"></source>
+          </video>
+    </div>
+  );
+}
+
+export default App;
